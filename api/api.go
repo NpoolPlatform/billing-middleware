fcf5398ae15a15e8ef3bbf199025161f0740e968
jakys <jakysing@163.com>
2025-03-18 20:01:58 +0800

    Fix api

--- api/api.go
+++ api/api.go
@@ -8,6 +8,7 @@ import (
 	subscription "github.com/NpoolPlatform/billing-middleware/api/subscription"
 	record "github.com/NpoolPlatform/billing-middleware/api/user/credit/record"
 	usersubscription "github.com/NpoolPlatform/billing-middleware/api/user/subscription"
+	usersubscriptionchange "github.com/NpoolPlatform/billing-middleware/api/user/subscription/change"
 	npool "github.com/NpoolPlatform/message/npool/billing/mw/v1"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
@@ -25,6 +26,7 @@ func Register(server grpc.ServiceRegistrar) {
 	exchange.Register(server)
 	usersubscription.Register(server)
 	record.Register(server)
+	usersubscriptionchange.Register(server)
 }
 
 func RegisterGateway(mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) error {
@@ -44,6 +46,9 @@ func RegisterGateway(mux *runtime.ServeMux, endpoint string, opts []grpc.DialOpt
 		return err
 	}
 	if err := record.RegisterGateway(mux, endpoint, opts); err != nil {
+		return err
+	}
+	if err := usersubscriptionchange.RegisterGateway(mux, endpoint, opts); err != nil {
 		return err
 	}
 	return nil
